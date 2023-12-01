10a0e11f6ba506a62ba7c52e703c8696da0be631
Jintumoni <jintu.kvs@gmail.com>
2023-12-01 22:48:19 +0530

    update version

--- deployer.sh
+++ deployer.sh
@@ -7,10 +7,10 @@ else
   VERSION="$MAJOR.$MINOR.$VERSION-SNAPSHOT"
 fi
 
-JAR=$(basename target/scala-2.12/*.jar .jar)"."$VERSION
+JAR=$(basename target/scala-2.11/*.jar .jar)"."$VERSION".jar"
 echo "JAR : $JAR"
 
 # Upload JAR to S3
 echo "Uploading " $JAR " to " s3://jintu-demo-s3/$JAR
-aws s3 cp target/scala-2.12/*.jar s3://jintu-demo-s3/$JAR
+aws s3 cp target/scala-2.11/*.jar s3://jintu-demo-s3/$JAR
 echo "Successfully upload $JAR to S3"
