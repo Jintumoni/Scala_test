10d373bce12373b8b144e4e541031bad5ec0a0e3
Jintumoni <jintu.kvs@gmail.com>
2023-12-02 10:54:07 +0530

    add region

--- deployer.sh
+++ deployer.sh
@@ -12,6 +12,6 @@ echo "JAR : $JAR"
 
 # Upload JAR to S3
 echo "Uploading " $JAR " to " s3://jintu-demo-s3/$JAR
-aws s3 cp path/to/artifacts/*.jar s3://jintu-demo-s3/$JAR
-echo "hello-from-github-actions" | aws s3 cp - s3://jintu-demo-s3/your-file-name.txt
+aws s3 cp path/to/artifacts/*.jar s3://jintu-demo-s3/$JAR --region ap-south-1
+echo "hello-from-github-actions" | aws s3 cp - s3://jintu-demo-s3/your-file-name.txt --region ap-south-1
 echo "Successfully upload $JAR to S3"
