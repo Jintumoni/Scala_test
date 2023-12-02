9f34f50033631ca408872117214cac26bd399961
Jintumoni <jintu.kvs@gmail.com>
2023-12-02 10:09:24 +0530

    comment s3 upload

--- deployer.sh
+++ deployer.sh
@@ -12,6 +12,6 @@ JAR=$(basename path/to/artifacts/*.jar .jar)"."$VERSION".jar"
 echo "JAR : $JAR"
 
 # Upload JAR to S3
-echo "Uploading " $JAR " to " s3://jintu-demo-s3/$JAR
-aws s3 cp target/scala-2.11/*.jar s3://jintu-demo-s3/$JAR
-echo "Successfully upload $JAR to S3"
+#echo "Uploading " $JAR " to " s3://jintu-demo-s3/$JAR
+#aws s3 cp target/scala-2.11/*.jar s3://jintu-demo-s3/$JAR
+#echo "Successfully upload $JAR to S3"
