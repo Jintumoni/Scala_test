fd07432d20f450c1faf2117ee58102ebaaa04a6b
Jintumoni <jintu.kvs@gmail.com>
2023-12-03 11:57:39 +0530

    test env

--- deployer.sh
+++ deployer.sh
@@ -1,17 +1,19 @@
-VERSION=$(git log --oneline | wc -l)
-if [[ "${GITHUB_REF}" == "refs/heads/master" ]]; then
-  echo "Deploying in prod env..."
-  VERSION="$MAJOR.$MINOR.$VERSION-RELEASE"
-else
-  echo "Deploying in dev env..."
-  VERSION="$MAJOR.$MINOR.$VERSION-SNAPSHOT"
-fi
+echo $ENV
 
-JAR=$(basename path/to/artifacts/*.jar .jar)"-"$VERSION".jar"
-echo "JAR : $JAR"
-
-# Upload JAR to S3
-echo "Uploading " $JAR " to " s3://jintu-demo-s3/$JAR
-aws s3 cp path/to/artifacts/*.jar s3://jintu-demo-s3/$JAR --region ap-south-1
-echo "hello-from-github-actions" | aws s3 cp - s3://jintu-demo-s3/your-file-name.txt --region ap-south-1
-echo "Successfully upload $JAR to S3"
+#VERSION=$(git log --oneline | wc -l)
+#if [[ "${GITHUB_REF}" == "refs/heads/master" ]]; then
+#  echo "Deploying in prod env..."
+#  VERSION="$MAJOR.$MINOR.$VERSION-RELEASE"
+#else
+#  echo "Deploying in dev env..."
+#  VERSION="$MAJOR.$MINOR.$VERSION-SNAPSHOT"
+#fi
+#
+#JAR=$(basename path/to/artifacts/*.jar .jar)"-"$VERSION".jar"
+#echo "JAR : $JAR"
+#
+## Upload JAR to S3
+#echo "Uploading " $JAR " to " s3://jintu-demo-s3/$JAR
+#aws s3 cp path/to/artifacts/*.jar s3://jintu-demo-s3/$JAR --region ap-south-1
+#echo "hello-from-github-actions" | aws s3 cp - s3://jintu-demo-s3/your-file-name.txt --region ap-south-1
+#echo "Successfully upload $JAR to S3"
